13ea45377a8e8ec24f2d001ba3fea2ab0a69d200
jimmi187 <project.jimi@proton.me>
2023-01-19 03:01:13 +0100

    regex added

---
+++ regex.py
@@ -0,0 +1,124 @@
+import re
+# Returns first instance of phone number pattern:
+def extract_phone(input):
+	phone_regex = re.compile(r'\b\d{3} \d{3}-\d{4}\b')
+	match = phone_regex.search(input)
+	if match:
+		return match.group()
+	return None
+
+# Returns all instances of phone number pattern in a list
+def extract_all_phones(input):
+	phone_regex = re.compile(r'\b\d{3} \d{3}-\d{4}\b')
+	return phone_regex.findall(input)
+
+# One way of checking if entire string is valid phone number:
+# def is_valid_phone(input):
+# 	phone_regex = re.compile(r'^\d{3} \d{3}-\d{4}$')
+# 	match = phone_regex.search(input)
+# 	if match:
+# 		return True
+# 	return False
+
+# Another way of doing the same thing, using the fullmatch method
+def is_valid_phone(input):
+	phone_regex = re.compile(r'\d{3} \d{3}-\d{4}')
+	match = phone_regex.fullmatch(input)
+	if match:
+		return True
+	return False
+
+import re
+
+url_regex = re.compile(r'(https?)://(www\.[A-za-z-]{2,256}\.[a-z]{2,6})([-a-zA-Z0-9@:%_\+.~#?&//=]*)')
+match = url_regex.search("https://www.my-website.com/bio?data=blah&dog=yes")
+print(f"Protocol: {match.group(1)}")
+print(f"Domain: {match.group(2)}")
+print(f"Everything Else: {match.group(3)}")
+print(match.groups())
+print(match.group())
+
+
+import re
+def parse_name(input):
+	name_regex = re.compile(r'^(Mr\.|Mrs\.|Ms\.|Mdme\.) (?P<first>[A-Za-z]+) (?P<last>[A-Za-z]+)$')
+	matches = name_regex.search(input)
+	print(matches.group())
+	print(matches.group('first'))
+	print(matches.group('last'))
+
+parse_name("Mrs. Tilda Swinton")
+
+
+# Without Verbose Flag...
+# pat = re.compile(r'^([a-z0-9_\.-]+)@([0-9a-z\.-]+)\.([a-z\.]{2,6})$')
+import re
+pattern = re.compile(r"""
+	^([a-z0-9_\.-]+)	#first part of email	
+	@					#single @ sign
+	([0-9a-z\.-]+)		#email provider
+	\.					#single period
+	([a-z\.]{2,6})$		#com, org, net, etc.
+""", re.X | re.I)
+
+match = pattern.search("ThomaS123@Yahoo.com")
+print(match.group())
+print(match.groups())
+
+
+text = "Last night Mrs. Daisy and Mr. white murdered Ms. Chow"
+
+pattern = re.compile(r'(Mr.|Mrs.|Ms.) ([a-z])[a-z]+', re.I)
+result = pattern.sub("\g<1> \g<2>", text)
+print(result)
+
+import re
+titles = [
+    "Significant Others (1987)",
+    "Tales of the City (1978)",
+    "The Days of Anna Madrigal (2014)",
+    "Mary Ann in Autumn (2010)",
+    "Further Tales of the City (1982)",
+    "Babycakes (1984)",
+    "More Tales of the City (1980)",
+    "Sure of You (1989)",
+    "Michael Tolliver Lives (2007)"
+]
+titles.sort()
+fixed_titles = []
+
+pattern = re.compile(r'(?P<title>^[\w ]+) \((?P<date>\d{4})\)')
+for book in titles:
+    # result = pattern.sub("\g<2> - \g<1>", book)
+    result = pattern.sub("\g<date> - \g<title>", book)
+
+    fixed_titles.append(result)
+fixed_titles.sort()
+print(fixed_titles)
+
+
+
+# Calling our functions a bunch of times...
+
+print(extract_phone("my number is 432 567-8976"))
+print(extract_phone("my number is 432 567-897622"))
+print(extract_phone("432 567-8976 asdjhasd "))
+print(extract_phone("432 567-8976"))
+
+print(extract_all_phones("my number is 432 567-8976 or call me at 345 666-7899"))
+print(extract_all_phones("my number is 432 56"))
+
+print(is_valid_phone("432 567-8976"))
+print(is_valid_phone("432 567-8976 ads"))
+print(is_valid_phone("asd 432 567-8976 d"))
+
+# import re 
+
+# def is_valid_time(st):
+#     time_regex = re.compile(r'^\d{1,2}:\d{2}$')
+#     match = time_regex.search(st)
+#     if match:
+#         return True
+#     return False
+    
+    
